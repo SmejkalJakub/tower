9b6642780a8352da82cf142d3c07be9ddaa77775
Jakub Smejkal <smejkal.jakub99@gmail.com>
2022-02-08 19:47:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+<a href="https://www.hardwario.com/"><img src="https://www.hardwario.com/ci/assets/hw-logo.svg" width="200" alt="HARDWARIO Logo" align="right"></a>
+
+# [PlatformIO](https://platformio.org) platform for HARDWARIO TOWER - Industrial IoT Kit
+
+[![Twitter](https://img.shields.io/twitter/follow/hardwario_en.svg?style=social&label=Follow)](https://twitter.com/hardwario_en)
+
+To start with firmware developement you should visit [our documentation](https://tower.hardwario.com/en/latest/firmware/basic-overview/).
+In the few sections you will install PlatformIO and build your first firmware with this platform.
+
+To install this platform with PlatformIO CLI please use
+
+``pio platform install hardwario-tower``
+
+You can also install our library twr-sdk:
+
+``pio lib --global install twr-sdk``
+
+To work with our devices we recommend to use [twr-skeleton](https://github.com/hardwario/twr-skeleton).
+You can find it in the examples. Or you can just clone this project and start working with the firmware.
+
+After you are familiar with the skeleton, you can update it to your needs.
+
+The platformio.ini file should look like this:
+```
+[platformio]
+default_envs = debug
+
+[env]
+platform = hardwario-tower
+board = core_module
+framework = stm32cube
+lib_deps = twr-sdk
+monitor_speed = 115200
+monitor_filters = default, send_on_enter
+monitor_flags = --echo
+
+[env:debug]
+upload_protocol = serial
+
+[env:release]
+build_flags = -D RELEASE
+upload_protocol = serial
+```
+
+---
+
+Made with &#x2764;&nbsp; by [**HARDWARIO s.r.o.**](https://www.hardwario.com) in Czech Republic.
---
+++ builder/main.py
@@ -0,0 +1,178 @@
+
+import click
+from platform import system
+from os.path import join, isdir
+from SCons.Script import AlwaysBuild, Builder, Default, DefaultEnvironment, COMMAND_LINE_TARGETS
+
+from platformio.util import get_serial_ports
+
+# Builder script for HARDWARIO TOWER 
+# To for more info about builder visit https://github.com/topics/platformio-platform
+# Also you can visit https://github.com/platformio/platform-ststm32, this platform uses stm32cube framework
+
+def BeforeUpload(target, source, env):
+    ports = get_serial_ports()
+        
+    if(len(ports) == 1):
+        env.Replace(UPLOAD_PORT=ports[0]['port'])
+    else:
+        click.secho("Please select upload port (line number):", fg='yellow')
+        for i, port in enumerate(ports):
+            click.secho(f"{i}: {port['port']}", fg='white')
+        
+        selected_port = input("")      
+        
+        if(not selected_port.isnumeric()):
+            for i, port in enumerate(ports):
+                if(port['port'] == selected_port):
+                    selected_port = i
+        else:
+            selected_port = int(selected_port)
+                                           
+        try:
+            click.secho("SELECTED PORT: " + ports[selected_port]['port'], fg='green')
+            env.Replace(UPLOAD_PORT=ports[selected_port]['port'])
+        except:
+            click.secho("WRONG PORT SELECTED! Please select line number or the port name", fg='red')
+            raise Exception("Upload port error")
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+board = env.BoardConfig()
+
+# A full list with the available variables
+# http://www.scons.org/doc/production/HTML/scons-user.html#app-variables
+env.Replace(
+    AR="arm-none-eabi-gcc-ar",
+    AS="arm-none-eabi-as",
+    CC="arm-none-eabi-gcc",
+    CXX="arm-none-eabi-g++",
+    GDB="arm-none-eabi-gdb",
+    OBJCOPY="arm-none-eabi-objcopy",
+    RANLIB="arm-none-eabi-gcc-ranlib",
+    SIZETOOL="arm-none-eabi-size",
+
+    ARFLAGS=["rc"],
+
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx)\s+(\d+).*",
+    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
+    SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+    
+    PROGNAME="firmware",
+    PROGSUFFIX=".elf"
+)
+
+env.Append(
+
+    BUILDERS=dict(
+        ElfToBin=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "binary",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".bin"
+        ),
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        )
+    )
+)
+
+project_config = env.GetProjectConfig()
+project_config.set('env:debug-jlink', 'debug_svd_path', env['PROJECT_CORE_DIR'] + "\platforms\hardwario-tower\stm32l0x3.svd")
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
+    target_firm = join("$BUILD_DIR", "${PROGNAME}.bin")
+else:
+    target_elf = env.BuildProgram()
+    target_firm = env.ElfToBin(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+
+AlwaysBuild(env.Alias("nobuild", target_firm))
+target_buildprog = env.Alias("buildprog", target_firm, target_firm)
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Target: Upload firmware
+#
+
+upload_source = target_firm
+upload_protocol = env.subst("$UPLOAD_PROTOCOL")
+upload_actions = []
+
+if(upload_protocol.startswith("serial")):
+
+    upload_actions = [
+        env.VerboseAction(BeforeUpload, "Looking for upload port..."),
+        env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")
+    ]
+
+    env.Replace(
+        UPLOADER="bcf",
+        UPLOADERFLAGS=[
+            "--device"
+        ],
+        UPLOADCMD="$UPLOADER flash $UPLOADERFLAGS $UPLOAD_PORT $SOURCE",
+    )
+
+elif(upload_protocol.startswith("jlink")):
+       
+    def _jlink_cmd_script(env, source):
+        build_dir = env.subst("$BUILD_DIR")
+        if not isdir(build_dir):
+            makedirs(build_dir)
+        script_path = join(build_dir, "upload.jlink")
+        commands = [
+            "h",
+            "loadbin %s, %s" % (source, board.get(
+                "upload.offset_address", "0x08000000")),
+            "r",
+            "q"
+        ]
+        with open(script_path, "w") as fp:
+            fp.write("\n".join(commands))
+        return script_path
+
+    env.Replace(
+        __jlink_cmd_script=_jlink_cmd_script,
+        UPLOADER="JLink.exe" if system() == "Windows" else "JLinkExe",
+        UPLOADERFLAGS=[
+            "-device", board.get("debug", {}).get("jlink_device"),
+            "-speed", env.GetProjectOption("debug_speed", "4000"),
+            "-if", ("jtag" if upload_protocol == "jlink-jtag" else "swd"),
+            "-autoconnect", "1",
+            "-NoGui", "1"
+        ],
+        UPLOADCMD='$UPLOADER $UPLOADERFLAGS -CommanderScript "${__jlink_cmd_script(__env__, SOURCE)}"'
+    )
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+upload = env.Alias("upload", upload_source, upload_actions)
+AlwaysBuild(upload)
+
+#
+# Target: Define targets
+#
+Default(target_firm)
